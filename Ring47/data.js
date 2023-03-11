0674e7066e4a775fa88dbef774dd10a536793b4b
flaaudwls <74338977+flaaudwls@users.noreply.github.com>
2023-03-11 12:12:48 -0500

    add to 47

---
+++ Ring47/data.js
@@ -0,0 +1,22 @@
+export const data = {
+  left_top_1: {
+    position: [-4.76, 9.98, -1.42],
+    scale: [1.13, 0.73, 1.47],
+    rotation: [0.03, 0.0, 0.56],
+  },
+  left_top_2: {
+    position: [-4.71, 9.91, 1.49],
+    scale: [1.13, 0.73, 1.47],
+    rotation: [0.03, 0.0, 0.56],
+  },
+  right_top_1: {
+    position: [4.75, 10.05, -1.42],
+    scale: [1.13, 0.73, 1.47],
+    rotation: [0.03, 0.03, -0.57],
+  },
+  right_top_2: {
+    position: [4.8, 9.98, 1.49],
+    scale: [1.13, 0.73, 1.47],
+    rotation: [0.03, 0.03, -0.57],
+  },
+};
