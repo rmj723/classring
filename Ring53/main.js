601746cd821e157b8903e6f95267d65dfa69832c
flaaudwls <74338977+flaaudwls@users.noreply.github.com>
2023-05-15 02:07:35 -0400

    fix side letters from 49 to 54

--- Ring53/main.js
+++ Ring53/main.js
@@ -37,7 +37,8 @@ const pos = {
   insideText: { x: 0.89, y: 41.74, z: 42.23 },
 };
 var ctx,
-  overflow = {};
+  overflow = {},
+  pivotUpdated = {};
 const p = { inside: { fontSize: 34, s: -20, e: 480, left: 0, top: 65 } };
 var delta = 300;
 
@@ -294,6 +295,10 @@ function changeText(t, side) {
         m.scale.set(0.8, 0.6, 1.28);
         m.scale.x = 1.7 - 0.1 * text.length;
 
+        if (!pivotUpdated[text[i]]) m.geometry.translate(0, 0, -0.8);
+        pivotUpdated[text[i]] = true;
+        m.scale.z = Math.abs(i - text.length / 2 + 0.5) * 0.1 + 1.12;
+
         m.visible = true;
         m.material = ring.material;
         charPos.right.push(m);
@@ -312,6 +317,11 @@ function changeText(t, side) {
         leftCurve.generatePose(m, i, 1.12);
         m.scale.set(0.8, 0.6, 1.28);
         m.scale.x = 1.7 - 0.1 * text.length;
+
+        if (!pivotUpdated[text[i]]) m.geometry.translate(0, 0, -0.8);
+        pivotUpdated[text[i]] = true;
+        m.scale.z = Math.abs(i - text.length / 2 + 0.5) * 0.1 + 1.12;
+
         m.visible = true;
         m.material = ring.material;
         charPos.left.push(m);
